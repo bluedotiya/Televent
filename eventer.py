d534e54efeb86e2769c388ef5adc88b9a6102e7b
bluedotiya <75704012+bluedotiya@users.noreply.github.com>
2021-04-12 20:01:16 +0300

    Content counter now update in an instant, old content counter now initate with current folder file count, main and sleep functions added.

--- eventer.py
+++ eventer.py
@@ -18,7 +18,7 @@ def main():
             new_file_name = paths[0].name
             print(new_file_name)
             old_content = content.__len__()
-            get(TELEGRAM_SEND_MESSAGE_URL.format(f'{new_file_name} fell in the net!  :D'), stream=True)
+            get(TELEGRAM_SEND_MESSAGE_URL.format(f'{new_file_name[:-4]} fell in the net!  :D'), stream=True)
             sleep(15)
         elif content.__len__() < old_content:
             old_content -= 1
@@ -26,5 +26,5 @@ def main():
             sleep(15)
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     main()
