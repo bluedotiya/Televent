8f4ae811ba1298922a89e7e7f2ebe404854f1499
bluedotiya <75704012+bluedotiya@users.noreply.github.com>
2021-04-12 19:48:57 +0300

    Content counter now update in an instant, old content counter now initate with current folder file count, main and sleep functions added.

--- eventer.py
+++ eventer.py
@@ -4,20 +4,27 @@ from pathlib import Path
 from requests import get
 from config import PATH, TELEGRAM_SEND_MESSAGE_URL
 
-old_content = 1
-while True:
-    content = os.listdir(PATH)
-    if content.__len__() > old_content:
-        paths = sorted(Path(PATH).iterdir(), key=os.path.getctime,reverse=True)
-        new_file_name = paths[0].name
-        print(new_file_name)
-        old_content = content.__len__()
-        get(TELEGRAM_SEND_MESSAGE_URL.format(f'{new_file_name} fell in the net!  :D'), stream=True)
-    elif content.__len__() < old_content:
-        old_content -= 1
-    time.sleep(15)
+
+def sleep(sec):
+    time.sleep(sec)
 
 
+def main():
+    old_content = os.listdir(PATH).__len__()
+    while True:
+        content = os.listdir(PATH)
+        if content.__len__() > old_content:
+            paths = sorted(Path(PATH).iterdir(), key=os.path.getctime, reverse=True)
+            new_file_name = paths[0].name
+            print(new_file_name)
+            old_content = content.__len__()
+            get(TELEGRAM_SEND_MESSAGE_URL.format(f'{new_file_name} fell in the net!  :D'), stream=True)
+            sleep(15)
+        elif content.__len__() < old_content:
+            old_content -= 1
+        else:
+            sleep(15)
 
 
-
+if __name__ == "__name__":
+    main()
