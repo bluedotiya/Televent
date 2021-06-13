62437d543a21a28fee85ab75acb711e908a08d7a
bluedotiya <75704012+bluedotiya@users.noreply.github.com>
2021-06-14 00:41:30 +0300

    Simple Telegram Eventer

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TOKEN = '1193665631:AAGZgkqfA19J7R5RQrJ46ajuy9m2F4buBsk'
-BASE_TELEGRAM_URL = f'https://api.telegram.org/bot{TOKEN}'
-TELEGRAM_SEND_MESSAGE_URL = BASE_TELEGRAM_URL + '/sendMessage?chat_id=613624132&text={}'
-TELEGRAM_SEND_STICKER_URL = BASE_TELEGRAM_URL + '/sendsticker?chat_id={}&sticker={}'
-PATH = "/home/pi/phishymal"
+#  Personal Info <DON'T SHARE>
+TOKEN = ''
+CHAT_ID = ''
+
+TELEGRAM_SEND_MESSAGE_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}' + '&text={}'
--- eventer.py
+++ eventer.py
@@ -1,29 +1,18 @@
-import os
-import time
-from pathlib import Path
 from requests import get
-from config import PATH, TELEGRAM_SEND_MESSAGE_URL
+from config import TELEGRAM_SEND_MESSAGE_URL
 
 
-def sleep(sec):
-    time.sleep(sec)
+def condition():
+    return True
+
+
+def message():
+    return "True"
 
 
 def main():
-    old_content = os.listdir(PATH).__len__()
-    while True:
-        content = os.listdir(PATH)
-        if content.__len__() > old_content:
-            paths = sorted(Path(PATH).iterdir(), key=os.path.getctime, reverse=True)
-            new_file_name = paths[0].name
-            print(new_file_name)
-            old_content = content.__len__()
-            get(TELEGRAM_SEND_MESSAGE_URL.format(f'{new_file_name[:-4]} fell in the net!  :D'), stream=True)
-            sleep(15)
-        elif content.__len__() < old_content:
-            old_content -= 1
-        else:
-            sleep(15)
+    if condition():
+        get(TELEGRAM_SEND_MESSAGE_URL.format(message()), stream=True)
 
 
 if __name__ == "__main__":
