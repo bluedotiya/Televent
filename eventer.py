90cb17f58eed18799dfc6057b304168aad117c65
bluedotiya <75704012+bluedotiya@users.noreply.github.com>
2021-06-14 01:02:49 +0300

    Simple Telegram Eventer

--- eventer.py
+++ eventer.py
@@ -3,16 +3,26 @@ from config import TELEGRAM_SEND_MESSAGE_URL
 
 
 def condition():
-    return True
-
-
-def message():
-    return "True"
+    message = ''
+    with open('/var/tmp/stats_gpu_fan_jq') as fan_speed_handle:
+       fan_speed = fan_speed_handle.readlines()[1]
+    with open('/var/tmp/stats_gpu_temp_jq') as gpu_temp_handle:
+        gpu_temp = gpu_temp_handle.readlines()[1]
+    if int(gpu_temp) > 55:
+        message += f"GPU is Overheating at: {gpu_temp}\t"
+    if int(fan_speed) > 30:
+        message += f"Fan speed is Under Performing at: {fan_speed}\t"
+    if message.__len__() > 0:
+        condition_sw = True
+    else:
+        condition_sw = False
+    return message, condition_sw
 
 
 def main():
-    if condition():
-        get(TELEGRAM_SEND_MESSAGE_URL.format(message()), stream=True)
+    msg, condition_var = condition()
+    if condition_var:
+        get(TELEGRAM_SEND_MESSAGE_URL.format(msg), stream=True)
 
 
 if __name__ == "__main__":
